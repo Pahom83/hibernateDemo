13fdbe33aac8e4fa2b3bb5555df6ce88ba647048
Pahom83 <pol_83@mail.ru>
2024-02-12 21:33:01 +0400

    Added JpaRepository

--- src/main/java/ru/netology/hibernatedemo/service/PersonService.java
+++ src/main/java/ru/netology/hibernatedemo/service/PersonService.java
@@ -1,24 +1,22 @@
 package ru.netology.hibernatedemo.service;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.netology.hibernatedemo.domain.Person;
 import ru.netology.hibernatedemo.exception.NotFoundException;
-import ru.netology.hibernatedemo.repository.PersonRepository;
+import ru.netology.hibernatedemo.repository.PersonRepositoryJpa;
 
 import java.util.List;
 @Service
+@AllArgsConstructor
 public class PersonService {
-    private final PersonRepository personRepository;
-
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
+    private final PersonRepositoryJpa repositoryJpa;
 
     public List<Person> getPersonByCity(String city) {
-        if (personRepository.findPersonByCity(city).isEmpty()){
+        if (repositoryJpa.findPersonByCityOfLiving(city).isEmpty()){
             throw new NotFoundException("Data not found");
         }
-        return personRepository.findPersonByCity(city);
+        return repositoryJpa.findPersonByCityOfLiving(city);
     }
 }
